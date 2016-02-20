679851fb8bb3cf18263c02a27938857cce737b6a
Vincent Alexander Saulys <valexandersaulys@gmail.com>
2016-02-19 20:41:17 -0500

    Theres an error in the tutorial, I cannot continue :(

--- app/models.py
+++ app/models.py
@@ -12,6 +12,37 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref="author", lazy="dynamic")
+
+
+    @property
+    def is_authenticated(self):
+        """
+        Misleading name, this should return True unless the object represents
+        a user that should not be allwoed to authenticate for some reason.
+        """
+        return True
+
+    @property
+    def is_active(self):
+        """
+        Should return true, unless the user is inactive.
+        """
+        return True
+
+    @property
+    def is_anonymous(self):
+        """
+        Should return True only for fake users that are not supposed to log into
+        the system.
+        """
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  #python2
+        except NameError:
+            return str(self.id)  #python3
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
-from app import app
-from flask import render_template, flash, redirect
+from app import app, db, lm, oid
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from .forms import LoginForm
+from .models import User
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    # the current_user global is set by Flask-Login
+    g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'nickname': 'Miguel'} # fake name
 
@@ -24,13 +36,64 @@ def index():
                            posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler  # This tells Flask-OpenIOd that this is our login view function
 def login():
+    # Checks to see if the user is logged in
+    if g.user is not None and g.user.is_authenticated:
+        # g is a global setup by Flask a place to store and share data during
+        # the life of a request. Here we'll be storing the logged in user.
+        
+        return redirect(url_for('index'))  # Using url_for() will enable Flask
+                                           # to build the necesary redirect.
+    # Get the login form
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login Request for OpenID="%s", remember="%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        # Remember the user log in
+        # remember_me boolean is stored into the flask session (not db.session)
+        # flask.session will store data made during that request and future
+        # requests made by the same client. data remains until explicity removed
+        session['remember_me'] = form.remember_me.data
+        
+        # This triggers the user authentication through Flask-OpenID
+        # OpenID authentication happens async, after which it'll call oid.after_login
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
 
+@oid.after_login
+def after_login(resp):  # resp is passed to afte_login with OpenID information
+    # First check is to look for validation, we need valid email
+    if resp.email is None or resp.email == "":
+        flask('Invalid Login, Please Try Again')
+        return redirect(url_for('login'))
+    
+    # Next, search for the user's email and if it isn't in there, we add it
+    user = User.query.filter_by(email=resp.email).first()
+    # This is just to formally add it to the database
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+
+    # Then we load the remember_me value from the Flask session
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+
+    # Then we call Flask-Login's login_user function to register this as valid
+    login_user(user, remember = remember_me)
+
+    # Then we redirect to the next page or the index page if a next page was
+    # not provided in the request
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
